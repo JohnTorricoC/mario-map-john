207564079c74ccb1efb90aa8b89f0729bc45592d
JohnTorricoC <dark.nightmare1309@gmail.com>
2021-03-17 14:50:35 -0400

    Create Search.py

---
+++ Search.py
@@ -0,0 +1,53 @@
+from copy import deepcopy
+from time import time
+import sys
+
+def getHijos(nodo, estados):
+    return estados.get(nodo)
+
+
+def busqueda_profundidad(inicial, objetivo, estados):
+    inicio_tiempo = time()
+    frontera = [[inicial]]
+    tam = 0
+    cont = 0
+    while frontera:
+        tam = max(sys.getsizeof(frontera), tam)
+        cont += 1
+        camino = frontera.pop()
+        nodo = camino[-1]
+        if nodo == objetivo:
+            fin_tiempo = time() - inicio_tiempo
+            print("Algoritmo tardo: %0.10f segundos." % fin_tiempo)
+            print("Maximo tamaño de frontera: ", tam, " bytes")
+            print("Se preciso de ", cont, " iteraciones para encontrar la solucion")
+            print("la solución tiene ", len(camino), " pasos")
+            return(camino)
+        else:
+            for h in getHijos(nodo, estados):
+                aux = deepcopy(camino)
+                aux.append(h)
+                frontera.append(aux)
+
+def busqueda_amplitud(inicial, objetivo, estados):
+    inicio_tiempo = time()
+    stack = [[inicial]]
+    tam = 0
+    cont = 0
+    while stack:
+        tam = max(sys.getsizeof(stack), tam)
+        cont += 1
+        camino = stack.pop(0)
+        nodo = camino[-1]
+        if nodo == objetivo:
+            fin_tiempo = time() - inicio_tiempo
+            print("Algoritmo tardo: %0.10f segundos." % fin_tiempo)
+            print("Maximo tamaño de del stack: ", tam, " bytes")
+            print("Se preciso de ", cont, " iteraciones para encontrar la solucion")
+            print("la solución tiene ", len(camino), " pasos")
+            return(cont)
+        else:
+            for h in getHijos(nodo, estados):
+                aux = deepcopy(camino)
+                aux.append(h)
+                stack.append(aux)
